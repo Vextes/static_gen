e167ed0bc8407aac91bd2560897f12e5a19632f8
vextes <sjfrang@gmail.com>
2025-12-17 12:30:41 -0600

    finish inline markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -77,3 +77,12 @@ def split_nodes_link(old_nodes):
         if og_text != "":
             result.append(TextNode(og_text, TextType.TEXT))
     return result
+
+def text_to_textnodes(text):
+    result = [TextNode(text, TextType.TEXT)]
+    result = split_nodes_delimiter(result, "**", TextType.BOLD)
+    result = split_nodes_delimiter(result, "_", TextType.ITALIC)
+    result = split_nodes_delimiter(result, "`", TextType.CODE)
+    result = split_nodes_image(result)
+    result = split_nodes_link(result)
+    return result
